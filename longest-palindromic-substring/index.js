8dc2d95f351585f2cb680e22ffe5942356fc1804
keep-promise <64321089+keep-promise@users.noreply.github.com>
2022-08-04 18:57:08 +0800

    Create index.js

---
+++ longest-palindromic-substring/index.js
@@ -0,0 +1,45 @@
+// @algorithm @lc id=5 lang=javascript 
+// @title longest-palindromic-substring
+// @test("babad")="bab"
+// @test("cbbd")="bb"
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+  return longestPalindromeOn3(s);
+};
+
+var isPalindrome = function(str, start, end) {
+  if (str[start] !== str[end]) return false;
+  start ++;
+  end --;
+  if (end >= start) {
+    return isPalindrome(str, start, end);
+  }
+  return true;
+}
+
+var isPalindrom2 = function(str) {
+  return str.split('').reverse().join() === str;
+}
+
+var longestPalindromeOn3 = function(s) {
+  var len = s.length;
+  var retstr = '';
+  var first = 0;
+  while (len > 0) {
+    first = 0;
+    while (first + len <= s.length) {
+      var substr = s.substring(first, first + len);
+      if (isPalindrom2(substr)) {
+        len = 0;
+        retstr = substr;
+        break;
+      }
+      first ++;
+    }
+    len--;
+  }
+  return retstr;
+};
